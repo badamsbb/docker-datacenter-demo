c833ef0d08667eb69adb92779dd7f945e9b5ae9c
Mehmet Ali Aydin <MehmetAli.Aydin@n11.com>
2017-02-24 01:15:10 +0300

    Tunings on resources and firewall disabled on workers

--- worker.sh
+++ worker.sh
@@ -1,4 +1,5 @@
 curl -SLf https://packages.docker.com/1.13/install.sh | sh
+sudo ufw disable
 until nc -w 1 192.168.35.10 443; do sleep 10; done
 until [ -f /vagrant/swarm-join.sh ]; do sleep 2; done
 sudo bash /vagrant/swarm-join.sh
